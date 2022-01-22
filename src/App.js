92828137e6553393d01373f007e9a418fe8a82bc
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-22 19:02:50 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import HomePage from './HomePage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div  >
+       <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage.component.jsx
@@ -0,0 +1,43 @@
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">hats</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">jackets</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">snickers</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">women</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">men</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">hats</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
