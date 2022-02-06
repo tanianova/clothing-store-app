08850449a5f944aee1a2d325d47ec4a3ad2cafa3
Tatiana Tikhonova <tanianova@yandex.ru>
2022-02-06 22:13:41 +0300

    added addItem, removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,10 @@
-export const addItemToCart = (cartItems, cartItemtoAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemtoAdd.id
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
-      cartItem.id === cartItemtoAdd.id
+      cartItem.id === cartItemToAdd.id
         ? {
             ...cartItem,
             quantity: cartItem.quantity + 1,
@@ -12,5 +12,21 @@ export const addItemToCart = (cartItems, cartItemtoAdd) => {
         : cartItem
     );
   }
-  return [...cartItems, { ...cartItemtoAdd, quantity: 1 }];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
