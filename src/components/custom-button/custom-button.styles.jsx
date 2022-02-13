aaf669fd3d09a692c17ee7ad4f881b0d2ec21dce
Tatiana Tikhonova <tanianova@yandex.ru>
2022-02-13 10:12:25 +0300

    added HOC withSpinner

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -4,18 +4,17 @@ const buttonStyles = css`
   background-color: black;
   color: white;
   border: none;
-
   &:hover {
     background-color: white;
     color: black;
     border: 1px solid black;
   }
 `;
+
 const invertedButtonStyles = css`
   background-color: white;
   color: black;
   border: 1px solid black;
-
   &:hover {
     background-color: black;
     color: white;
@@ -26,7 +25,6 @@ const invertedButtonStyles = css`
 const googleSignInStyles = css`
   background-color: #4285f4;
   color: white;
-
   &:hover {
     background-color: #357ae8;
     border: none;
@@ -37,12 +35,11 @@ const getButtonStyles = (props) => {
   if (props.isGoogleSignIn) {
     return googleSignInStyles;
   }
+
   return props.inverted ? invertedButtonStyles : buttonStyles;
 };
 
 export const CustomButtonContainer = styled.button`
-  display: flex;
-  justify-content: center;
   min-width: 165px;
   width: auto;
   height: 50px;
@@ -50,13 +47,11 @@ export const CustomButtonContainer = styled.button`
   line-height: 50px;
   padding: 0 35px 0 35px;
   font-size: 15px;
-  background-color: black;
-  color: white;
   text-transform: uppercase;
   font-family: "Open Sans Condensed";
   font-weight: bolder;
-  border: none;
   cursor: pointer;
-
+  display: flex;
+  justify-content: center;
   ${getButtonStyles}
 `;
